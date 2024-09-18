14a16938efc890f05584eaecfc5249af9d0b8c14
walterEon <130522615+walterEon@users.noreply.github.com>
2024-09-18 17:24:28 -0500

    Update Carrito.js

--- src/pages/Carrito/Carrito.js
+++ src/pages/Carrito/Carrito.js
@@ -1,1 +1,12 @@
+import './Carrito.css';
 
+function Carrito() {
+
+    return(
+        <div className='carrito-page'>
+            <h1>Mi carrito</h1>
+        </div>
+    )
+}
+
+export default Carrito;
